6101fb9a4f178490a130c7bb55ee98628f71fdac
tsteelematc <tsteele@madisoncollege.edu>
2022-11-03 13:29:57 -0500

    Showing quizzes...
    In desired shape ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[];
+}
+
+interface QuestionDisplay {
+  questionText: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,5 +22,15 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const data = this.quizSvc.loadQuizzes();
     console.log(data);
+
+    this.quizzes = data.map((x: any) => ({
+      quizName: x.name
+      , quizQuestions: x.questions.map((y: any) => ({
+        questionText: y.name
+      }))
+    }));
+    console.log(this.quizzes);
   }
+
+  quizzes: QuizDisplay[] = [];
 }
