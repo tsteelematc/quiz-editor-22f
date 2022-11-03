47a55a4851fed2b9b568fc50aed6cd8e4250b25c
tsteelematc <tsteele@madisoncollege.edu>
2022-11-03 13:04:32 -0500

    Calling our service...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'quiz-editor';
+export class AppComponent implements OnInit {
+ 
+  constructor(private quizSvc: QuizService) {}
+
+  ngOnInit() {
+    const data = this.quizSvc.loadQuizzes();
+    console.log(data);
+  }
 }
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,27 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  loadQuizzes = (): any[] => {
+
+    const quizzesFromWeb = [
+      {
+        name: 'Quiz 1'
+        , questions: [
+          {
+            name: 'Question 1'
+          }
+          , {
+            name: 'Question 2'
+          }
+        ]
+      }
+      , {
+        name: 'Quiz 2'
+        , questions: []
+      }
+    ];
+
+    return quizzesFromWeb;
+  };
 }
