995385e643ee607a0e260c59386e6a717c99b0c9
tsteelematc <tsteele@madisoncollege.edu>
2022-12-06 13:23:09 -0600

    Details from left mostly working...
    Would have to hack the HTML to get it to animate the first time a quiz is selected...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, ShapeForSavingEditedQuizzes, ShapeForSavingNewQuizzes } from './quiz.service';
 
+import {
+  trigger
+  , transition
+  , animate
+  , keyframes
+  , style
+} from '@angular/animations';
+
 interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
@@ -16,7 +24,29 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ marginLeft: '-30px', offset: 0.0 }),
+          style({ marginLeft: '-20px', offset: 0.25 }),
+          style({ marginLeft: '-10px', offset: 0.5 }),
+          style({ marginLeft: '-5px', offset: 0.75 }),
+          style({ marginLeft: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]  
 })
 export class AppComponent implements OnInit {
  
@@ -67,6 +97,7 @@ export class AppComponent implements OnInit {
 
   selectQuiz = (quizToSelect: QuizDisplay) => {
     this.selectedQuiz = quizToSelect;
+    this.detailsAnimationState = "finalPosition";
   };
 
   addNewQuiz = () => {
@@ -242,4 +273,10 @@ export class AppComponent implements OnInit {
     && !x.markedForDelete
     && this.generateNaiveChecksum(x) != x.naiveChecksum
   ;
+
+  detailsAnimationState = "leftPosition";
+
+  detailsAnimationDone = () => {
+    this.detailsAnimationState = "leftPosition";
+  };
 }
